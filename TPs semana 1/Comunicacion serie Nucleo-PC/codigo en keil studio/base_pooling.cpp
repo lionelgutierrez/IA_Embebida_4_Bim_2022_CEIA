384aad564ba71a718f49b119d804eff7a0b3f422
Lionel <lionelgutierrez@gmail.com>
2022-09-07 18:07:01 -0300

    Agregado codigo en keil studio

---
+++ TPs semana 1/Comunicacion serie Nucleo-PC/codigo en keil studio/base_pooling.cpp
@@ -0,0 +1,31 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "platform/mbed_thread.h"
+
+// millisegundos durante los cuales mando a dormir al micro
+#define SLEEP_RATE     100 ms
+#define MAXIMUM_BUFFER_SIZE 1
+
+int main()
+{
+    DigitalOut myled(LED2);
+    BufferedSerial uartUsb(USBTX, USBRX);    
+    
+    char receivedChar[MAXIMUM_BUFFER_SIZE] = {0};
+    uint32_t num;
+
+     while (1) {
+        if( uartUsb.readable() ) {
+                myled = 1;
+                num = uartUsb.read(receivedChar, sizeof(receivedChar));
+                if (num) {
+                    uartUsb.write(receivedChar, num);
+                }
+                myled = 0;
+        }
+     }
+}
